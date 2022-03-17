ab5ffd7cf7843cbfeeebb8079c624b4c28f86454
armanozak <levent.ozak@gmail.com>
2022-03-17 21:13:36 +0100

    Fix i18n and Tailwind config

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -3,24 +3,7 @@ import { TRANSLOCO_SCOPE } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-hello',
-  template: `
-    <div class="container" *transloco="let t; read: translocoScope">
-      <form>
-        <mat-form-field appearance="fill">
-          <mat-label>{{ t('_label.receiver') }}</mat-label>
-          <input
-            matInput
-            placeholder="Eg. Angular"
-            name="receiver"
-            [(ngModel)]="receiver"
-          />
-        </mat-form-field>
-      </form>
-
-      <h1>{{ t('greeting', { receiver }) }}</h1>
-    </div>
-  `,
-  styleUrls: ['./hello.component.scss'],
+  templateUrl: './hello.component.html',
 })
 export class HelloComponent {
   receiver = 'world';
--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -5,7 +5,7 @@ import {
   TranslocoLoader,
   TRANSLOCO_CONFIG,
   translocoConfig,
-  TranslocoModule
+  TranslocoModule,
 } from '@ngneat/transloco';
 import { Injectable, NgModule } from '@angular/core';
 import { environment } from '../environments/environment';
@@ -15,24 +15,27 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   constructor(private http: HttpClient) {}
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`${environment.baseUrl}/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(
+      `${environment.baseUrl}/assets/i18n/${lang}.json`
+    );
   }
 }
 
 @NgModule({
-  exports: [ TranslocoModule ],
+  exports: [TranslocoModule],
   providers: [
     {
       provide: TRANSLOCO_CONFIG,
       useValue: translocoConfig({
         availableLangs: ['en'],
         defaultLang: 'en',
+        fallbackLang: 'en',
         // Remove this option if your application doesn't support changing language in runtime.
         reRenderOnLangChange: true,
         prodMode: environment.production,
-      })
+      }),
     },
-    { provide: TRANSLOCO_LOADER, useClass: TranslocoHttpLoader }
-  ]
+    { provide: TRANSLOCO_LOADER, useClass: TranslocoHttpLoader },
+  ],
 })
 export class TranslocoRootModule {}
