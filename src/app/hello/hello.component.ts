1f57aa27ef3b05e32613438ec42c113e2adc9f1b
armanozak <levent.ozak@gmail.com>
2022-03-17 21:19:17 +0100

    Use OnPush

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,9 +1,10 @@
-import { Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { TRANSLOCO_SCOPE } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HelloComponent {
   receiver = 'world';
